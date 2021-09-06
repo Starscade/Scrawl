effe8a090245fa65bde3173851c999e2023af757
Angus Sheehan <8022349+Starscade@users.noreply.github.com>
2021-09-06 10:57:11 -0700

    Create work.js

---
+++ work.js
@@ -0,0 +1,33 @@
+'use strict';
+const CASH={'name':'Scrawl_CASH','cache':[
+  './index.htm',
+  './ico.png',
+  './x.js',
+  './scrawl.js',
+  './ui.css',
+  './particl.css'
+]};
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    const off=await caches.match(e.request);
+    caches.open(CASH['Scrawl_CASH']).then(cash=>{
+      const on=fetch(e.request).then(resp=>{
+        cash.put(e.request,resp.clone());
+        return resp;
+      });
+    });
+    if(on){
+      return on;
+    }else{
+      return off;
+    }
+  );
+});
+self.addEventListener('install',e=>{
+  console.log('Installed!');
+  e.waitUntil((async ()=>{
+    const cash=await caches.open(CASH['Scrawl_CASH']);
+    await cash.addAll(CASH['cache']);
+    console.log('Cached!');
+  })());
+});
