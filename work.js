97d603ed9f1dc18820e2da3f7c31cf3a4e1e36c0
Angus Sheehan <8022349+Starscade@users.noreply.github.com>
2021-09-06 12:31:31 -0700

    Update work.js

--- work.js
+++ work.js
@@ -1,14 +1,18 @@
 'use strict';
 const CASH={'name':'Scrawl_CASH','cache':[
-  './index.htm',
-  './ico.png',
-  './x.js',
-  './scrawl.js',
-  './ui.css',
-  './particl.css'
+  '/index.htm',
+  '/ico.png',
+  '/x.js',
+  '/scrawl.js',
+  '/ui.css',
+  '/particl.css'
 ]};
 self.addEventListener('fetch',e=>{
   console.log(e.request);
+  e.respondWith(async ()=>{
+    const off=await caches.match(e.request);
+    if(off){return off;}
+  });
 });
 self.addEventListener('install',e=>{
   console.log('Installed!');
